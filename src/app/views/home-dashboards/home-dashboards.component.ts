5ad6c81fb862cd407c465338918edd36eae545e0
LeticiaTP <karlaleticiatavares@gmail.com>
2024-06-29 16:58:26 -0300

    feat(dashboard) adiciona sidenav e saudação

--- src/app/views/home-dashboards/home-dashboards.component.ts
+++ src/app/views/home-dashboards/home-dashboards.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
-
+import { ChangeDetectionStrategy, signal } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-home-dashboards',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, MatExpansionModule, MatSidenavModule],
   templateUrl: './home-dashboards.component.html',
-  styleUrl: './home-dashboards.component.css'
+  styleUrl: './home-dashboards.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeDashboardsComponent {
-
+  readonly panelOpenState = signal(false);
 }
